e266751c19275bd2627127d048457a2922fc2943
aguaman123 <anderson.guaman@udla.edu.ec>
2025-04-16 16:16:59 -0500

    mejora login

--- src/app/presentacion/pages/login/login.component.ts
+++ src/app/presentacion/pages/login/login.component.ts
@@ -3,10 +3,11 @@ import { AuthService } from '../../../infraestructura/service/auth.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,ButtonModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
